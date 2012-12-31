2efac24242ac19b268c791d9100044a401c6c276
yegle <cnyegle@gmail.com>
2012-12-30 22:40:57 -0500

    Added: constants

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+LISTENQ = 1024
