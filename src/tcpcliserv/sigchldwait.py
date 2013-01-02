a94d58c17bb61b7a6bf335773eb065b4ad139391
yegle <cnyegle@gmail.com>
2013-01-02 18:53:59 -0500

    Add: signal handler for child process

---
+++ src/tcpcliserv/sigchldwait.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+import os
+import logging
+
+def sig_chld(signo, ignored):
+    pid, exit_status = os.wait()
+    logging.warning("child %d terminated, exit_status %d" % (pid, exit_status))
