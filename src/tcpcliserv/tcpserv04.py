b24209a3f4d8c8157865c3055ad08e9edd4a6957
yegle <cnyegle@gmail.com>
2013-01-03 02:52:37 -0500

    Add: tcpserv04.py

---
+++ src/tcpcliserv/tcpserv04.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+import os
+import sys
+import socket
+import signal
+import errno
+sys.path.append('..')
+from misc import constants as const
+from misc import tools
+from sigchldwaitpid import sig_chld
+
+if __name__ == '__main__':
+    listenfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+
+    # empty string means INADDR_ANY
+    servaddr = ('', const.SERV_PORT)
+
+    listenfd.bind(servaddr)
+    listenfd.listen(const.LISTENQ)
+
+    signal.signal(signal.SIGCHLD, sig_chld)
+
+    while True:
+        try:
+            connfd, remote_addr = listenfd.accept()
+        except socket.error as e:
+            if e.errno == errno.EINTR:
+                continue
+            else:
+                tools.err_sys(e, msg="accept error")
+
+        if not os.fork():
+            # close listen fd in child process
+            # not actual close, just minus its reference count
+            # by one
+            listenfd.close()
+            tools.str_echo(connfd)
+            connfd.close()
+            sys.exit(0)
+
+        connfd.close()
