a1bef86e269f1369453c174dde67f2b7d00f93a7
Yuchen Ying <yegle@uga.edu>
2013-01-08 13:53:26 -0500

    Add: placeholder for `unp ls`

--- unp/main.py
+++ unp/main.py
@@ -25,10 +25,12 @@ class UNPCommand(object):
             self.show()
         elif self.args.operation == 'edit':
             self.edit()
+        elif self.args.operation == 'ls':
+            self.ls()
 
     def parse_arguments(self):
         self.parser.add_argument('operation', choices=['run', 'show',
-                                                       'edit'])
+                                                       'edit', 'ls'])
         self.parser.add_argument('target')
         self.parser.add_argument('args', nargs=argparse.REMAINDER)
         self.args = self.parser.parse_args()
@@ -38,17 +40,19 @@ class UNPCommand(object):
             self.args.target
         )
 
+    def get_module(self):
         target = self.args.target
         if target.endswith('.py') or target.endswith('.c'):
             module_name = '.'.join(target.split('.')[:-1]).replace('/', '.')
         else:
             module_name = target.replace('/', '.')
-        self.main = import_module('%s.%s' % ('unp', module_name), package='unp').main
+        return import_module('%s.%s' % ('unp', module_name), package='unp').main
 
     def run(self):
         prog = '%s %s %s' % (os.path.basename(sys.argv[0]),
                              self.args.operation, self.args.target)
-        self.main(prog, self.args.args)
+
+        self.get_module()(prog, self.args.args)
 
     def show(self):
         print(self.file)
@@ -57,5 +61,8 @@ class UNPCommand(object):
         editor = os.environ.get('EDITOR', None) or 'vim'
         # XXX: security problem?
         subprocess.call([editor, self.file])
+
+    def ls(self):
+        raise NotImplementedError('Please be patient, or send pull-request!')
 
 command = UNPCommand()
