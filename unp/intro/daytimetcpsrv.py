aa18f37115dd484de4d091c7ff4e2f16060808fe
yegle <cnyegle@gmail.com>
2013-01-13 13:16:34 -0500

    Mod: daytimetcpsrv* to support specifying port

--- unp/intro/daytimetcpsrv.py
+++ unp/intro/daytimetcpsrv.py
@@ -6,16 +6,41 @@ from __future__ import (unicode_literals, absolute_import,
 
 import socket
 import sys
+import argparse
 from ..misc import constants as const
 from datetime import datetime
 
+def parse(prog, args):
+    parser = argparse.ArgumentParser(
+        prog=prog,
+        description='Daytime TCP Server for IPv4',
+    )
+    parser.add_argument(
+        'ip',
+        help='IP Address to listen to. (default 0.0.0.0)',
+        default='0.0.0.0',
+        nargs='?'
+    )
+    parser.add_argument(
+        'port',
+        help='Port to listen to.',
+        type=int,
+        default=13,
+        nargs='?'
+    )
+    return parser.parse_args(args)
 
 def main(prog, args):
+    parsed_args = parse(prog, args)
+
     listenfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+
+    if parsed_args.ip == '0.0.0.0':
+        ip = ''
 
     # Listen on any local IP, and port 13
     # Note: empty address '' represents INADDR_ANY
-    listen_addr = ('', 13)
+    listen_addr = (ip, parsed_args.port)
 
     listenfd.bind(listen_addr)
     listenfd.listen(const.LISTENQ)
--- unp/intro/daytimetcpsrv1.py
+++ unp/intro/daytimetcpsrv1.py
@@ -10,13 +10,37 @@ import logging
 from ..misc import constants as const
 from datetime import datetime
 
+def parse(prog, args):
+    parser = argparse.ArgumentParser(
+        prog=prog,
+        description='Daytime TCP Server for IPv4, with log output',
+    )
+    parser.add_argument(
+        'ip',
+        help='IP Address to listen to. (default 0.0.0.0)',
+        default='0.0.0.0',
+        nargs='?'
+    )
+    parser.add_argument(
+        'port',
+        help='Port to listen to. (default 13)',
+        type=int,
+        default=13,
+        nargs='?'
+    )
+    return parser.parse_args(args)
 
 def main(prog, args):
+    parsed_args = parse(prog, args)
+
     listenfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+
+    if parsed_args.ip == '0.0.0.0':
+        ip = ''
 
     # Listen on any local IP, and port 13
     # Note: empty address '' represents INADDR_ANY
-    listen_addr = ('', 13)
+    listen_addr = (ip, parsed_args.port)
 
     listenfd.bind(listen_addr)
     listenfd.listen(const.LISTENQ)
